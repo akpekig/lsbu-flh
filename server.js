934cd7be908f886e299049dcda449ba425e29071
Gillian Akpeki <57150149+akpekig@users.noreply.github.com>
2021-04-25 17:58:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,30 +3,33 @@ var app = express();
 var bodyParser=require("body-parser");
 var document = { };
 var submissionsList = { };
+var caseDocuments = { };
+let extraData;
+let caseID;
 
 const host = '0.0.0.0';
-const port = process.env.PORT || 5000; //port number (5000) is neither necessary nor constant per application.
+const port = process.env.PORT || 5000;
 
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb+srv://<username>:<password>@<mongodbcluster>.mongodb.net/<database>?retryWrites=true&w=majority"; //replace parameters as needed.
+const url = "mongodb+srv://LSBULC:dyg3LmLdwdyc@legalclinicsubmissions.y0jds.mongodb.net/database?retryWrites=true&w=majority";
 const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
-  service: 'gmail', // change email provider as needed 
+  service: 'gmail',
   auth: {
-    user: 'email@email.com',
-    pass: 'password' // naturally, replace both with your real credentials or an application-specific password
+    user: 'lsbuflh@gmail.com',
+    pass: 'LSBUflh1234' // naturally, replace both with your real credentials or an application-specific password
   }
 });
 
 const mailOptions = {
-  from: 'from@mail.com',
-  to: 'to@mail.com',
+  from: 'lsbuflh@gmail.com',
+  to: 'lsbuflh@gmail.com',
   subject: 'LSBU Family Law Hub Application',
   text: 'A client has recently submitted a form. Visit your dashboard to view details and assign the case to your students.',
-  html:'<div style="font-family: \'Arial\', sans-serif;"><h1>A client has recently submitted a form.</h1><p> Click <a href="dashboard-link" alt="Admin Dashboard">here</a> to view details and assign the case to your students.</p></div><body>',
+  html:'<div style="font-family: \'Arial\', sans-serif;"><h1>A client has recently submitted a form.</h1><p> Click <a href="#" alt="Admin Dashboard">here</a> to view details and assign the case to your students.</p></div><body>',
 };
 
 async function add() {
@@ -58,11 +61,86 @@ async function load() {
          console.log(err.stack);
      }
 
-     /*finally {
-        await client.close();
-    }*/
 }
 
+async function studentLoad() {
+    try {
+         await client.connect();
+         console.log("Connected correctly to server");
+         const db = client.db("database");
+         caseDocuments = await db.collection("submissions").find( { "_id": require('mongodb').ObjectID(caseID) } ).toArray();
+         console.log(caseDocuments);
+         console.log("Retrieved");
+   } catch (err) {
+         console.log(err.stack);
+     }
+
+}
+async function updateDetail() {
+    try {
+         console.log("Connected correctly to server");
+         const db = client.db("database");
+         console.log('Assigning case ' + caseID + ' to students.');
+         await db.collection("submissions").findOneAndUpdate(
+           { "_id": require('mongodb').ObjectID(caseID) },
+           { "$set": { "extraDetails": extraDetails } },
+           { returnNewDocument: true });
+         console.log("Case assigned.");
+         res.redirect('/dashboard-admin');
+   } catch (err) {
+         console.log(err.stack);
+     }
+}
+
+async function assign() {
+    try {
+         console.log("Connected correctly to server");
+         const db = client.db("database");
+         console.log('Assigning case ' + caseID + ' to students.');
+         await db.collection("submissions").findOneAndUpdate(
+           { "_id": require('mongodb').ObjectID(caseID) },
+           { "$set": { "status": "assigned" } },
+           { returnNewDocument: true });
+         console.log("Case assigned.");
+         res.redirect('/dashboard-admin');
+   } catch (err) {
+         console.log(err.stack);
+     }
+}
+
+async function close() {
+    try {
+         //await client.connect();
+         console.log("Connected correctly to server");
+         const db = client.db("database");
+         console.log('Closing case ' + caseID)
+         await db.collection("submissions").findOneAndUpdate(
+           { "_id": require('mongodb').ObjectID(caseID) },
+           { "$set": { "status": "closed" } },
+           { returnNewDocument: true });
+         console.log("Case closed.");
+   } catch (err) {
+         console.log(err.stack);
+     }
+
+}
+
+async function delete() {
+    try {
+         //await client.connect();
+         console.log("Connected correctly to server");
+         const db = client.db("database");
+         console.log('Deleting case ' + caseID)
+         await db.collection("submissions").findOneAndDelete(
+           { "_id": require('mongodb').ObjectID(caseID) },
+           {}
+         );
+         console.log("Case deleted.");
+   } catch (err) {
+         console.log(err.stack);
+     }
+
+}
 
 
 app.set('view engine', 'ejs');
@@ -73,8 +151,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // index page
 app.get('/', function(req, res) {
     res.render('pages/index');
-    //console.log(req.formPage.formJSON);
 });
+
 // submission
 app.post('/submit', function(req, res){
     let submitted = 0;
@@ -96,7 +174,47 @@ app.post('/submit', function(req, res){
 // admin dashboard
 app.get('/dashboard-admin', function(req, res) {
     load().catch(console.dir);
-    res.render('pages/dashboard-admin', { submissions: submissionsList });
+    res.render('pages/dashboard-admin', { submissions: submissionsList});
+});
+
+
+app.post('/dashboard-admin/assign', function(req, res) {
+  console.log(req.body)
+    caseID = req.body.caseID;
+    let buttonAction = req.body.buttonAction;
+    if (buttonAction == 'assign'){
+      assign().catch(console.dir);
+      res.redirect('/dashboard-admin');
+    } else if (buttonAction == 'close') {
+      close().catch(console.dir);
+      res.redirect('/dashboard-admin');
+    } else if (buttonAction == 'delete') {
+      return res.render('/pages/delete', { caseID: caseID });
+    }
+
+});
+
+// admin dashboard delete
+app.post('/delete', function(req, res) {
+    caseID = req.body.caseID;
+    delete().catch(console.dir);
+    res.redirect('/dashboard-admin');
+});
+
+// student dashboard
+app.get('/dashboard-student', function(req, res) {
+    res.render('pages/dashboard-student');
+});
+app.post('/dashboard-student/find', function(req, res) {
+    caseID = req.body.caseID;
+    studentLoad().catch(console.dir);
+    res.render('pages/find', { documents: caseDocuments });
+});
+app.post('/dashboard-student/find/updateDetail', function(req, res) {
+    extraDetails = req.body.extraDetails;
+    studentLoad().catch(console.dir);
+    res.render('pages/find', { documents: caseDocuments });
 });
 
 app.listen(port, host);
+console.log('5000 is the magic port');
